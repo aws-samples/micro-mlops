965be27fac7989db63735f99187f9a53491d1de1
starship-cloud <94441374+starship-cloud@users.noreply.github.com>
2022-09-10 12:23:24 +0800

    Delete install.sh

--- cicd-templates/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#git clone https://gitlab.cloudstart-metaverse.com/mlops-aws/gitlab-ci/cicd-templates.git
-#git pull
-
-HOOK_DIR=/var/opt/gitlab/gitaly/custom_hooks/pre-receive.d
-mkdir $HOOK_DIR -p
-cp -r custom_hooks/pre-receive.d/. $HOOK_DIR/
-chown git:git $HOOK_DIR/.
